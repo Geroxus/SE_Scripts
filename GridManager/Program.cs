67240e0f7fd28513a69df25e0490127d1416c289
Geroxus <goldto@web.de>
2024-12-22 00:42:54 +0100

    auto refactor

--- GridManager/Program.cs
+++ GridManager/Program.cs
@@ -1,21 +1,6 @@
-﻿using Sandbox.Game.EntityComponents;
-using Sandbox.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using SpaceEngineers.Game.ModAPI.Ingame;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using VRage;
-using VRage.Collections;
-using VRage.Game;
-using VRage.Game.Components;
-using VRage.Game.GUI.TextPanel;
-using VRage.Game.ModAPI.Ingame;
-using VRage.Game.ModAPI.Ingame.Utilities;
-using VRage.Game.ObjectBuilders.Definitions;
+using Sandbox.ModAPI.Ingame;
 using VRageMath;
 
 namespace IngameScript
@@ -29,9 +14,8 @@ namespace IngameScript
 
         private double _lastSpeed;
         
-        private MyCommandLine _commandLine = new MyCommandLine();
         private readonly Commander<IMyShipController> _bridgeCommander;
-        private int _targetVelocity = 0;
+        private int _targetVelocity;
         private readonly List<IMyThrust> _forwardThrusters = new List<IMyThrust>();
         private readonly List<IMyThrust> _reverseThrusters = new List<IMyThrust>();
 
